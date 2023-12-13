112d4e55ddc5ad30e91f3ae87272ba31919c3b38
izumin2000 <kanata.muses.c.2010613@gmail.com>
2023-12-14 06:00:31 +0900

    [Add] ページの追加

--- iniadmatch/urls.py
+++ iniadmatch/urls.py
@@ -3,8 +3,11 @@ from iniadmatch import views
 
 
 urlpatterns = [
-    path("", views.TopView.as_view(), name='top'),
-    path('login/', views.CustomLoginView.as_view(), name='login'),
-    path('logout/', views.CustomLogoutView.as_view(), name='logout'),
-    path('auth/', include('social_django.urls', namespace='social')),  
+    path("", views.TopView.as_view(), name="top"),
+    path("schedule/<int:schedule>", views.ScheduleView.as_view(), name="schedule"),
+    path("search", views.SearchView.as_view(), name="search"),
+    path("setting", views.SettingView.as_view(), name="setting"),
+    path("login/", views.CustomLoginView.as_view(), name="login"),
+    path("logout/", views.CustomLogoutView.as_view(), name="logout"),
+    path("auth/", include("social_django.urls", namespace="social")),  
 ]
--- iniadmatch/views.py
+++ iniadmatch/views.py
@@ -1,21 +1,35 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import render, redirect
-from django.views import View
+from django.views import generic
+from .models import *
 
-
-class TopView(View):
+class TopView(generic.ListView) :
     template_name = 'iniadmatch/top.html'
+    model = Schedule
 
-    def get(self, request, *args, **kwargs):
-        if not request.user.is_authenticated:
+    def get(self, request, *args, **kwargs) :
+        if not request.user.is_authenticated :
             return redirect('login')
 
         return render(request, self.template_name)
 
 
-class CustomLoginView(LoginView):
+class ScheduleView(generic.DetailView) :
+    template_name = 'iniadmatch/schedule.html'
+    model = Schedule
+
+    
+class SearchView(generic.TemplateView) :
+    template_name = 'iniadmatch/search.html'
+
+
+class SettingView(generic.TemplateView) :
+    template_name = 'iniadmatch/setting.html'
+
+
+class CustomLoginView(LoginView) :
     template_name = 'iniadmatch/top.html'
 
 
-class CustomLogoutView(LogoutView):
+class CustomLogoutView(LogoutView) :
     template_name = 'iniadmatch/top.html'
