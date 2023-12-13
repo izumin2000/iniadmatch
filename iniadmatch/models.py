6abec977cc021f1fc2a7c892d9f37b1b1deb84b5
izumin2000 <kanata.muses.c.2010613@gmail.com>
2023-12-14 05:27:37 +0900

    [DB Change]

--- iniadmatch/models.py
+++ iniadmatch/models.py
@@ -3,61 +3,55 @@ from social_django.models import UserSocialAuth
 
 
 
-class Account(models.Model):
-    name = models.CharField(max_length=255)
+class Account(models.Model) :
+    name = models.CharField(default="", max_length=255, null=True)
     usersocialauth = models.OneToOneField(UserSocialAuth, on_delete=models.CASCADE)
 
-    def get_gmail(self):
+    def get_gmail(self) :
         return self.usersocialauth.uid
 
 
-class Student(models.Model):
-    account = models.OneToOneField(Account, on_delete=models.CASCADE)
-
-    def get_name(self):
-        return self.account.name
-    
-    def get_gmail(self):
-        return self.account.usersocialauth.uid
+class Tag(models.Model) :
+    name = models.CharField(default="", max_length=255)
 
 
-class Tag(models.Model):
-    name = models.CharField(max_length=255)
-
-
-class Teacher(models.Model):
+class Teacher(models.Model) :
     account = models.OneToOneField(Account, on_delete=models.CASCADE)
     tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
 
-    def get_name(self):
+    def get_name(self) :
         return self.account.name
     
-    def get_gmail(self):
+    def get_gmail(self) :
         return self.account.usersocialauth.uid
         
 
-class Schedule(models.Model):
+class Schedule(models.Model) :
     teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
-    date = models.DateField()
+    week = models.IntegerField(default=0)
+    start = models.TimeField(null=True)
+    end = models.TimeField(null=True)
 
-    def get_teacher_name(self):
+    def get_teacher_name(self) :
         return self.teacher.account.name
     
+    def get_week_name(self) :
+        week_list = ["月曜日", "火曜日", "水曜日", "木曜日", "金曜日"]
+        return week_list[self.week]
+    
 
-class Booking(models.Model):
-    student = models.ForeignKey(Student, on_delete=models.CASCADE)
-    schedule = models.ForeignKey(Schedule, on_delete=models.CASCADE)
-    date = models.DateField()
-    comment = models.TextField()
+class Booking(models.Model) :
+    student = models.ForeignKey(Account, on_delete=models.CASCADE)
+    schedule = models.OneToOneField(Schedule, on_delete=models.CASCADE)
 
-    def get_student_name(self):
-        return self.student.account.name
+    def get_student_name(self) :
+        return self.student.name
 
-    def get_teacher_name(self):
+    def get_teacher_name(self) :
         return self.schedule.teacher.account.name
     
-    def get_schedule_data(self):
+    def get_schedule_data(self) :
         return self.schedule.date
     
-    def get_teacher_tags(self):
+    def get_teacher_tags(self) :
         return self.schedule.teacher.tag
