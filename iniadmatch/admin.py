8fae67b34637b73bd333ca0b3e8ddc7346b14af2
izumin2000 <kanata.muses.c.2010613@gmail.com>
2024-01-02 18:03:08 +0900

    [Add] adminの有効化

--- iniadmatch/admin.py
+++ iniadmatch/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Account, Tag, Teacher, Schedule, Booking
 
-# Register your models here.
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ('name', 'get_gmail')
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ('get_name', 'get_gmail', 'tag')
+
+@admin.register(Schedule)
+class ScheduleAdmin(admin.ModelAdmin):
+    list_display = ('get_teacher_name', 'get_week_name', 'start', 'end')
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('get_student_name', 'get_teacher_name', 'get_schedule_data', 'get_teacher_tags')
