0a5e84e3f520791c9a1ab3316f75e1706794b71e
izumin2000 <kanata.muses.c.2010613@gmail.com>
2024-01-05 04:48:07 +0900

    [Add] routineの登録と削除の実装

--- iniadmatch/views.py
+++ iniadmatch/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 from django.views import generic
 from .models import *
 import re
+import datetime
 
 def isTeacher(user):
     return not bool(re.match(r'^s\df\d{9}$', user.username))
@@ -36,15 +37,47 @@ class SearchView(generic.TemplateView) :
 
 class SettingView(generic.TemplateView) :
     template_name = 'iniadmatch/setting.html'
-
+    def get(self, request, *args, **kwargs):
+        d = {}
+        teacher = request.user
+        d["viewname"] = teacher.account.name
+        for week in range(5) :
+            teacher_routine = Routine.objects.filter(teacher__account__user=teacher, week=week)
+            if teacher_routine :
+                teacher_routine = teacher_routine.first()
+                d[f'week{week}_start'], d[f'week{week}_end'] = teacher_routine.start, teacher_routine.end
+            else :
+                d[f'week{week}_start'], d[f'week{week}_end'] = "", ""
+                
+        return render(request, self.template_name, d)
+        
     def post(self, request, *args, **kwargs):
-        viewname = request.POST.get('viewname', '')
+        d = {}
+        
+        post = request.POST
+        viewname = post.get('viewname', '')
+        d["viewname"] = viewname
         
         teacher = request.user
         account_rec = Account.objects.filter(user=teacher).first()
         account_rec.name = viewname
         account_rec.save()
-        return render(request, self.template_name, {'viewname': viewname})
+        for week in range(5) :
+            start, end = post.get(f'week{week}_start', ''), post.get(f'week{week}_end', '')
+            teacher_routine = Routine.objects.filter(teacher__account__user=teacher, week=week)
+            if ("" in [start, end]) :
+                if teacher_routine :
+                    teacher_routine.delete()
+                
+            else :
+                teacher_routine, _ = Routine.objects.get_or_create(teacher__account__user=teacher, week=week)
+                teacher_routine.teacher = teacher.account.teacher
+                teacher_routine.start = datetime.datetime.strptime(start, "%H:%M").time()
+                teacher_routine.end = datetime.datetime.strptime(end, "%H:%M").time()
+                teacher_routine.save()
+                d[f'week{week}_start'], d[f'week{week}_end'] = start, end
+                
+        return render(request, self.template_name, d)
     
     
 class CustomLoginView(LoginView) :
