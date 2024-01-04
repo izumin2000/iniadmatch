fa34190e2a1d4db2e6c0fda36f33c14801ca8973
izumin2000 <kanata.muses.c.2010613@gmail.com>
2024-01-05 02:44:31 +0900

    [Change] setupのアップデート

--- iniadmatch/management/commands/setup.py
+++ iniadmatch/management/commands/setup.py
@@ -2,7 +2,8 @@ from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 from iniadmatch.models import *
 from datetime import time, datetime
-
+from config.local_settings import SUB_DEV_USER_PASSWORD
+from django.utils import timezone
 
 class Command(BaseCommand) :
     def handle(self, *args, **options) :
@@ -11,24 +12,44 @@ class Command(BaseCommand) :
         Tag.objects.all().delete()
         Routine.objects.all().delete()
         
-        student_user = User.objects.get(username = "s3f102300023")
-        teacher_user = User.objects.get(username = "minecraftiniad")
+        teacher1_user = User.objects.get(username = "minecraftiniad")
+        teacher2_user = User.objects.get(username = "kanapan2.poo")
+        student1_user = User.objects.get(username = "s3f102300023")
+        if User.objects.filter(username = "s3f10230024") :
+            student2_user = User.objects.create_user(username = "s3f102300024", password=SUB_DEV_USER_PASSWORD)
         
-        Account.objects.create(name = "泉川 叶多", user = student_user).save()
-        teacher_account = Account.objects.create(name = "矢代", user = teacher_user)
-        teacher_account.save()
+        Account.objects.create(name = "泉川 叶多", user = student1_user).save()
+        teacher1_account = Account.objects.create(name = "矢代", user = teacher1_user)
+        teacher2_account = Account.objects.create(name = "石川", user = teacher2_user)
+        teacher1_account.save()
+        teacher2_account.save()
         
-        teacher = Teacher.objects.create(account = teacher_account)
-        teacher.save()
+        teacher1 = Teacher.objects.create(account = teacher1_account)
+        teacher2 = Teacher.objects.create(account = teacher2_account)
+        teacher1.save()
+        teacher2.save()
         
-        tag_1 = Tag.objects.create(name = "CS", teacher = teacher) 
-        tag_2 = Tag.objects.create(name = "研究の相談", teacher = teacher) 
+        tag_1 = Tag.objects.create(name = "CS", teacher = teacher1) 
+        tag_2 = Tag.objects.create(name = "研究の相談", teacher = teacher1)
         tag_1.save()
         tag_2.save()
         
-        Routine.objects.create(teacher = teacher, week = 0, start = time(13, 0), end = time(14, 30)).save()
-        Routine.objects.create(teacher = teacher, week = 2, start = time(14, 45), end = time(16, 15)).save()
+        teacher1 = Teacher.objects.get(account=teacher1_account)
         
-        Schedule.objects.create(teacher = teacher, start = datetime(2024, 1, 15, 13, 0), end = datetime(2024, 1, 15, 14, 30)).save()
-        Schedule.objects.create(teacher = teacher, start = datetime(2024, 1, 17, 14, 45), end = datetime(2024, 1, 17, 16, 15)).save()
+        Routine.objects.create(teacher = teacher1, week = 0, start = time(13, 0), end = time(14, 30)).save()
+        Routine.objects.create(teacher = teacher1, week = 2, start = time(14, 45), end = time(16, 15)).save()
+        
+
+        utc_timezone = timezone.utc
+        start_datetime1 = datetime(2024, 1, 15, 13, 0, tzinfo=utc_timezone)
+        end_datetime1 = datetime(2024, 1, 15, 14, 30, tzinfo=utc_timezone)
+        Schedule.objects.create(teacher=teacher1, start=start_datetime1, end=end_datetime1).save()
+
+        start_datetime2 = datetime(2024, 1, 17, 14, 45, tzinfo=utc_timezone)
+        end_datetime2 = datetime(2024, 1, 17, 16, 15, tzinfo=utc_timezone)
+        Schedule.objects.create(teacher=teacher1, start=start_datetime2, end=end_datetime2).save()
+
+        start_datetime3 = datetime(2024, 1, 16, 14, 45, tzinfo=utc_timezone)
+        end_datetime3 = datetime(2024, 1, 16, 16, 15, tzinfo=utc_timezone)
+        Schedule.objects.create(teacher=teacher2, start=start_datetime3, end=end_datetime3).save()
         
