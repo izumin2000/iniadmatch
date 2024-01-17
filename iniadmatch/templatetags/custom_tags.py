3cf2a8654334fdf85c7b3db185bc16bf38c3be8e
izumin2000 <kanata.muses.c.2010613@gmail.com>
2024-01-17 21:14:03 +0900

    [Add] GoogleCalenderURLの実装

--- iniadmatch/templatetags/custom_tags.py
+++ iniadmatch/templatetags/custom_tags.py
@@ -1,5 +1,6 @@
 from django import template
-from django.template.defaultfilters import date 
+from django.template.defaultfilters import date
+from datetime import datetime
 
 register = template.Library()
 
@@ -13,7 +14,15 @@ def display_schedule_time(day, start, end):
     else:
         return ""
 
-
+@register.simple_tag
+def url_schedule_time(day, start, end):
+    if day and start and end:
+        formatted_str = f"{day.strftime('%Y%m%d')}T{start.strftime('%H%M%S')}00/{day.strftime('%Y%m%d')}T{end.strftime('%H%M%S')}00"
+        return formatted_str
+    else:
+        return ""
+    
+    
 @register.filter
 def tags_to_comma_separated_string(tags):
     return ', '.join(tag.name for tag in tags)
